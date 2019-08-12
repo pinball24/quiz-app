0c75d544c04e2a095f175122f926420544c36af8
Brian <briankulaga21@gmail.com>
2019-08-12 15:45:35 -0500

    added questions layout

---
+++ index.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const STORE = [
+    {
+       number: 1,
+       text: `What season did the red wedding take place in?`,
+       answer1: `Season 1`,
+       answer2: `Season 3`,
+       answer3: `Season 5`,
+       answer4: `Season 7` 
+    },
+
+    {
+        number: 2,
+        text: `What is Jon Snow's real name?`,
+        answer1: `Ned Stark`,
+        answer2: `Robert Baratheon`,
+        answer3: `Aegon Targaryen`,
+        answer4: `Rhaegar Targaryen`
+    },
+
+    {
+        number: 3,
+        text: `What is the name of Arya's sword?`,
+        answer1: `Pointy`,
+        answer2: `Ice`,
+        answer3: `Needle`,
+        answer4: `Stabby`
+    },
+
+    {
+        number: 4,
+        text: `Who had molten gold poured over there head?`,
+        answer1: `Tony Blackburn`,
+        answer2: `The Hound`,
+        answer3: `Gregor Clegane`,
+        answer4: `Viserys Targaryen`
+    },
+
+    {
+        number: 5,
+        text: `What's house Stark's motto?`,
+        answer1: `"Winter is coming"`,
+        answer2: `"Hear me roar"`,
+        answer3: `"We do not sow"`,
+        answer4: `"Fire and blood"`
+    },
+
+    {
+        number: 6,
+        text: `Who is know as "The Kingslayer"?`,
+        answer1: `Arya Stark`,
+        answer2: `Robert Bartheon`,
+        answer3: `Jamie Lannister`,
+        answer4: `Jon Snow`
+    },
+
+    {
+        number: 7,
+        text: `Which character said "When you play the game of thrones, you win or you die."?`,
+        answer1: `Cersei Lannister`,
+        answer2: `Tyrion Lannister`,
+        answer3: `Robb Stark`,
+        answer4: `Olenna Tyrell`
+    },
+
+    {
+        number: 8,
+        text: `What was the name of Jon Snow's direwolf?`,
+        answer1: `Summer`,
+        answer2: `Ghost`,
+        answer3: `Shaggydog`,
+        answer4: `Nymeria`
+    },
+
+    {
+        number: 9,
+        text: `Which territory is Mance Rayder recognized as king?`,
+        answer1: `Winterfell`,
+        answer2: `Beyond the wall`,
+        answer3: `King's Landing`,
+        answer4: `The iron islands`
+    },
+
+    {
+        number: 10,
+        text: `In the series finale, who ends up becoming the king or queen?`,
+        answer1: `Jon Snow`,
+        answer2: `Jamie Lannister`,
+        answer3: `Sansa Stark`,
+        answer4: `Bran Stark`
+    }
+];
+
+const ANSWERS = [
+    `Season 3`,
+    `Aegon Targaryen`,
+    `Needle`,
+    `Stabby`,
+    `Viserys Targaryen`,
+    `"Winter is coming"`,
+    `Jamie Lanniser`,
+    `Cersei Lannister`,
+    `Ghost`,
+    `Beyond the wall`,
+    `Bran Stark`
+];
